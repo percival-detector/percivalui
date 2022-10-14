ac8009ffcf99b18582685f24440a07581c225a16
William Nichols <none@users.noreply.github.com>
2022-10-14 12:03:26 +0100

    tool_merge_acq to cope with zero-based files

--- user_scripts/tool_merge_acq.py
+++ user_scripts/tool_merge_acq.py
@@ -9,6 +9,7 @@ import os.path;
 import h5py;
 import numpy as np;
 import collections;
+import re;
 
 # change this to insert ref cols with value zero at the left if you want.
 numrefcols = 32;
@@ -20,6 +21,7 @@ def splitGFC(pval):
 
     return imgGn, imgFn, imgCrs;
 
+# filename1 and filename2 do need to be in the right order
 def merge_files_and_save(filename1, filename2, outfile, delete=True, split=False):
   f1 = h5py.File(filename1, "r");
   f2 = h5py.File(filename2, "r");
@@ -106,17 +108,21 @@ def main():
       if args.label in filename and not "combined" in filename:
         allfiles[filename] = 1;
 
+    # in the odin-data update to 1.8, the numbering started at zero instead of 1.
     count = 0;
     for filename1 in allfiles.keys():
       if filename1.endswith("01.h5"):
-        filename2 = filename1.replace("01.h5", "02.h5");
-        if os.path.exists(os.path.join(indir,filename2)) and count < args.maxfiles:
-          outname = filename1.replace("000001.h5", "combined.h5");
+        filename_oth = filename1.replace("01.h5", "02.h5");
+        if(filename_oth not in allfiles):
+          filename_oth = filename1.replace("01.h5", "00.h5");
+          filename1, filename_oth = filename_oth, filename1;
+        if os.path.exists(os.path.join(indir,filename1)) and os.path.exists(os.path.join(indir,filename_oth)) and count < args.maxfiles:
+          outname = re.sub("\d{6}\.h5", "combined.h5", filename1);
           if False and os.path.exists(os.path.join(outdir, outname)):
             print("already done ", outname);
           else:
-            print("Merging{} {} and {} into\n *** {}".format("+s" if args.split else "",filename1, filename2, outname));
-            merge_files_and_save(os.path.join(indir, filename1), os.path.join(indir, filename2),
+            print("Merging{} {} and {} into\n *** {}".format("+s" if args.split else "",filename1, filename_oth, outname));
+            merge_files_and_save(os.path.join(indir, filename1), os.path.join(indir, filename_oth),
                 os.path.join(outdir, outname), args.delete, args.split);
             count += 1;
 
