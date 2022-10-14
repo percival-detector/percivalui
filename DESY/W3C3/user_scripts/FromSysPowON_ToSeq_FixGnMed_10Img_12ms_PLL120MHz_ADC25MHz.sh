34e567e8f71c30be7c6ad432d9b6806b4c367358
William Nichols <none@users.noreply.github.com>
2022-10-14 15:21:43 +0100

    add support for FixGn modes

--- DESY/W3C3/user_scripts/FromSysPowON_ToSeq_FixGnMed_10Img_12ms_PLL120MHz_ADC25MHz.sh
+++ DESY/W3C3/user_scripts/FromSysPowON_ToSeq_FixGnMed_10Img_12ms_PLL120MHz_ADC25MHz.sh
@@ -8,10 +8,9 @@ echo "change biases from standard-after-PowON-status to $2"
 percival-hl-scan-setpoints -i 08_1_CurrentBiases_ON_ready3T -f $2 -n 2 -d 500
 percival-hl-set-channel -c VS_Vref_DB -v 0
 
-echo "Load ADC25MHz, PLL120MHz, 3T,PGAB SequentialMode, 10 Images, 12ms integration"
+echo "Load ADC25MHz, PLL120MHz, Gn1,PGAB SequentialMode, 10 Images, 12ms integration"
 percival-hl-configure-clock-settings -i ./DESY/W3C3/config/01_Clock_Settings/ClockSettings_N06_120MHz_ACD25MHz.ini
-# there is a questionmark about whether this should be N15 or N18
-percival-hl-configure-chip-readout-settings -i ./DESY/W3C3/config/02_Chip_Readout_Settings/ChipReadoutSettings_N15_3T_ADC25MHz_PLL120MHz_SEQUENTIAL.ini
+percival-hl-configure-chip-readout-settings -i ./DESY/W3C3/config/02_Chip_Readout_Settings/ChipReadoutSettings_N18_PGAB_Gn1_ADC25MHz_PLL120MHz_SEQUENTIAL.ini   
 percival-hl-configure-system-settings -i ./DESY/W3C3/config/03_System_Settings/SystemSettings_N15_pixel_10Img_12ms_SEQUENTIAL.ini
 
 echo RESET DATA SYNCH STATUS...
@@ -31,7 +30,7 @@ percival-hl-configure-sensor-debug -i ./DESY/W3C3/config/04_Sensor_Settings/Sens
 percival-hl-system-command -c enter_acquisition_armed_status
 
 echo  "DONE"
-echo  "PLL120MHz,ADC25MHz, 10img, 12ms, 3T, PGAB"
+echo  "PLL120MHz,ADC25MHz, 10img, 12ms, Gn1, PGAB"
 echo  "SeqMod,dmuxSELswitching SeqMode,dmuxSELswitching"
 
 else
