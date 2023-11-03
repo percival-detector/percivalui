e3290afbfe869f25e7bf935bade5f77fb7ec18bd
William Nichols <none@users.noreply.github.com>
2023-11-03 10:34:33 +0000

    add config files for 125MHz operation

---
+++ DESY/W3C3/user_scripts/DLS_FSI10_FromSysPowON_ToSeq_FixGnHi_10Img_12ms_PLL125MHz_ADC25MHz.sh
@@ -0,0 +1,53 @@
+
+percival-hl-configure-setpoints -i ./DESY/W3C3/config/05_Spreadsheets/DLS_Setpoint_Definitions.xls
+
+percival-hl-system-command -c stop_acquisition
+percival-hl-system-command -c exit_acquisition_armed_status
+
+echo "change biases from standard-after-PowON-status to FSI10_FixGnHi"
+percival-hl-scan-setpoints -i 08_1_CurrentBiases_ON_ready3T -f FSI10_FixGnHi -n 2 -d 500
+
+echo "Load ADC25MHz, PLL125MHz, 3T,PGAB SequentialMode, 10 Images, 12ms integration"
+percival-hl-configure-clock-settings -i ./DESY/W3C3/config/01_Clock_Settings/ClockSettings_N22_125MHz_ACD25MHz.ini
+# there is a questionmark about whether this should be N15 or N18
+percival-hl-configure-chip-readout-settings -i ./DESY/W3C3/config/02_Chip_Readout_Settings/ChipReadoutSettings_N15_3T_ADC25MHz_PLL120MHz_SEQUENTIAL.ini
+percival-hl-configure-system-settings -i ./DESY/W3C3/config/03_System_Settings/SystemSettings_N15_pixel_10Img_12ms_SEQUENTIAL.ini
+
+echo RESET DATA SYNCH STATUS...
+# EXIT ARMED STATUS
+percival-hl-system-command -c exit_acquisition_armed_status
+# ASSERT CPNI FLAGS IN DEBUG REGISTERS
+percival-hl-configure-sensor-debug -i ./DESY/W3C3/config/04_Sensor_Settings/SensorDebug_002_SET_CPNI.ini
+# TOGGLE CPNI_EXT
+percival-hl-set-system-setting -s ADVANCED_Enable_CPNI_EXT -v 1
+percival-hl-set-system-setting -s ADVANCED_CPNI_EXT_options -v 1
+percival-hl-set-system-setting -s ADVANCED_CPNI_EXT_options -v 2
+percival-hl-set-system-setting -s ADVANCED_CPNI_EXT_options -v 1
+percival-hl-set-system-setting -s ADVANCED_Enable_CPNI_EXT -v 0
+# DEASSERT ALL FLAGS IN DEBUG REGISTERS
+percival-hl-configure-sensor-debug -i ./DESY/W3C3/config/04_Sensor_Settings/SensorDebug_000_SAFE_START.ini
+# ENTER ARMED STATUS
+percival-hl-system-command -c enter_acquisition_armed_status
+
+# the reset must be done an even number of occurrences else the mez board will mis-arrange the frames.
+echo RESET DATA SYNCH STATUS AGAIN...
+# EXIT ARMED STATUS
+percival-hl-system-command -c exit_acquisition_armed_status
+# ASSERT CPNI FLAGS IN DEBUG REGISTERS
+percival-hl-configure-sensor-debug -i ./DESY/W3C3/config/04_Sensor_Settings/SensorDebug_002_SET_CPNI.ini
+# TOGGLE CPNI_EXT
+percival-hl-set-system-setting -s ADVANCED_Enable_CPNI_EXT -v 1
+percival-hl-set-system-setting -s ADVANCED_CPNI_EXT_options -v 1
+percival-hl-set-system-setting -s ADVANCED_CPNI_EXT_options -v 2
+percival-hl-set-system-setting -s ADVANCED_CPNI_EXT_options -v 1
+percival-hl-set-system-setting -s ADVANCED_Enable_CPNI_EXT -v 0
+# DEASSERT ALL FLAGS IN DEBUG REGISTERS
+percival-hl-configure-sensor-debug -i ./DESY/W3C3/config/04_Sensor_Settings/SensorDebug_000_SAFE_START.ini
+# ENTER ARMED STATUS
+percival-hl-system-command -c enter_acquisition_armed_status
+
+echo  "DONE"
+echo  "PLL125MHz, ADC25MHz, 10img, 12ms, 3T, PGAB"
+echo  "SeqMod,dmuxSELswitching SeqMode,dmuxSELswitching"
+
+
