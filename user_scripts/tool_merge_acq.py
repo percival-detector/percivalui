9d8d9c83fd023bb011be34b7fe15315c908e72c5
William Nichols <none@users.noreply.github.com>
2024-04-30 11:51:03 +0100

    elaborate the merge tool with a descramble option

--- user_scripts/tool_merge_acq.py
+++ user_scripts/tool_merge_acq.py
@@ -10,9 +10,15 @@ import h5py;
 import numpy as np;
 import collections;
 import re;
+from liveview.descrambler import *;
 
-# change this to insert ref cols with value zero at the left if you want.
-numrefcols = 32;
+def addrefcols(frames):
+  # change this to insert ref cols with value zero at the left if you want.
+  numrefcols = 32;
+
+  refcols = np.zeros((frames.shape[0], frames.shape[1], numrefcols), dtype=frames.dtype);
+  return np.concatenate((refcols, frames), axis=2);
+
 
 def splitGFC(pval):
     imgGn= pval >> 13;
@@ -21,8 +27,22 @@ def splitGFC(pval):
 
     return imgGn, imgFn, imgCrs;
 
+def saveGCF(of, dset, ileaved, split, descramble):
+    gain = crs = fine = None;
+    if (descramble):
+        (gain, crs,fine) = descramble_to_GnCrsFn(ileaved);
+    elif (split):
+      #  (gain, fine, crs) = splitGFC(ileaved);
+        (gain, crs, fine) = aggregate_to_GnCrsFn(ileaved);
+
+    if (gain is not None):
+      of.create_dataset(dset + "-gain", data=addrefcols(gain));
+      of.create_dataset(dset + "-coarse", data=addrefcols(crs));
+      of.create_dataset(dset + "-fine", data=addrefcols(fine));
+
+
 # filename1 and filename2 do need to be in the right order
-def merge_files_and_save(filename1, filename2, outfile, delete=True, split=False):
+def merge_files_and_save(filename1, filename2, outfile, delete, split, descramble):
   f1 = h5py.File(filename1, "r");
   f2 = h5py.File(filename2, "r");
   of = h5py.File(outfile, "w");
@@ -33,34 +53,43 @@ def merge_files_and_save(filename1, filename2, outfile, delete=True, split=False
       if shape1[1] != 1484 or shape1[2] != 1408:
         print("oh dear wrong shape on input", shape1);
         exit(1);
-      # we are going to expand the frame to have ref cols at the left
+
       numfr1 = shape1[0];
       numfr2 = f2[dset].shape[0];
-      outshape = (numfr1+numfr2, 1484, 1408 + numrefcols);
+      outshape = (numfr1+numfr2, 1484, 1408);
 
-      dtp = "float32" if dset=="ecount" else "uint16";
-
-      output = np.zeros(outshape, dtype=dtp);
+      ileaved = np.zeros(outshape, f1[dset].dtype);
 
       for i in range(0, numfr1):
-        output[i*2,:,numrefcols:] = f1[dset][i,:,:];
+        ileaved[i*2,:,:] = f1[dset][i,:,:];
 
       for i in range(0, numfr2):
-        output[i*2+1,:, numrefcols:] = f2[dset][i,:,:];
+        ileaved[i*2+1,:,:] = f2[dset][i,:,:];
 
-     # output[0::2,:,:] = f1[:,:,:]; would also work
+     # ileaved[0::2,:,:] = f1[:,:,:]; would also work
+      saveGCF(of, dset, ileaved, split, descramble);
+      ileaved = addrefcols(ileaved);
 
-      of.create_dataset(dset, data = output);
+      of.create_dataset(dset, data = ileaved);
 
-      if(split):
-        if dset in ["data", "reset"]:
-          gain, fine, crs = splitGFC(output);
-          of.create_dataset(dset + "-gain", data=gain);
-          of.create_dataset(dset + "-coarse", data=crs);
-          of.create_dataset(dset + "-fine", data=fine);
-          
+  for dset in ["info"]:
+    if dset in f1:
+      shape1 = f1[dset].shape;
 
-  # should also do info field here.
+      numfr1 = shape1[0];
+      numfr2 = f2[dset].shape[0];
+      outshape = (numfr1+numfr2, shape1[1], shape1[2]);
+
+      ileaved = np.zeros(outshape, dtype=np.uint8);
+
+      for i in range(0, numfr1):
+        ileaved[i*2,:,:] = f1[dset][i,:,:];
+
+      for i in range(0, numfr2):
+        ileaved[i*2+1,:,:] = f2[dset][i,:,:];
+
+      of.create_dataset(dset, data = ileaved);
+
 
   if delete:
     print("deleting {} and its pair".format(filename1));
@@ -76,7 +105,8 @@ def options():
     parser.add_argument("-i", "--indir", default="/dls/detectors/Percival/captures", help="folder for h5 files input\n  (default /dls/detectors/Percival/captures)")
     parser.add_argument("--no-delete", action="store_false", help="delete original capture files after merge (default on)", default=True, dest="delete")
     parser.add_argument("-o", "--outdir", default="", help="folder for h5 files output (default same as indir)")
-    parser.add_argument("-s", "--split", default=False, action="store_true", help="add datasets for g,f,c too");
+    parser.add_argument("-s", "--split", default=False, action="store_true", help="add datasets for g,c,f too");
+    parser.add_argument("-d", "--descramble", default=False, action="store_true", help="add datasets for g,c,f and descramble image data into them");
     parser.add_argument("-v", "--verbose", help="verbose logging (default F)", action="store_true", default=False);
     parser.add_argument("-m", "--maxfiles", help="max number of files to create", type=int, default=100000);
 
@@ -102,6 +132,12 @@ def main():
       print ("invalid directory ", args.outdir);
       exit(1);
 
+    split_desc = "";
+    if args.split:
+      split_desc = "+s";  
+    elif args.descramble:
+      split_desc = "+d";
+
 
     allfiles = collections.OrderedDict();
     for filename in sorted(os.listdir(indir)):
@@ -121,9 +157,9 @@ def main():
           if False and os.path.exists(os.path.join(outdir, outname)):
             print("already done ", outname);
           else:
-            print("Merging{} {} and {} into\n *** {}".format("+s" if args.split else "",filename1, filename_oth, outname));
+            print("Merging{} {} and {} into\n *** {}".format(split_desc,filename1, filename_oth, outname));
             merge_files_and_save(os.path.join(indir, filename1), os.path.join(indir, filename_oth),
-                os.path.join(outdir, outname), args.delete, args.split);
+                os.path.join(outdir, outname), args.delete, args.split, args.descramble);
             count += 1;
 
 
