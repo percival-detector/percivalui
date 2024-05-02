3ca40bae2016bdfd9da5292a748531e2c7e3d891
William Nichols <none@users.noreply.github.com>
2024-05-02 11:08:10 +0100

    replace isAlive() in bgui, add nixt script and acquisition script
    These are some fixes that resulted from a trip to Desy, where they
    have rh9. Toko Hirono left a custom acquisition script at Desy which
    I have committed.
    
    The function isAlive() has been removed from the threading library,
    and is_alive() is an alternative which works in python2/3.
    The nixt script does nothing, or is a debug tool which is useful so
    I decided to add it to github.
    
    Also fixed the return codes of the wiener on/off scripts.

--- user_scripts/wiener/mainswitchOFF.sh
+++ user_scripts/wiener/mainswitchOFF.sh
@@ -6,6 +6,11 @@ WIENER_IP=172.23.16.179
 
 echo Switching the WIENER Power Supply OFF
 
-# read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
-
 snmpset -v 2c -m +WIENER-CRATE-MIB -c guru $WIENER_IP sysMainSwitch.0 i 0
+
+FAIL=$?
+if [ $FAIL -eq 0 ]; then
+echo Wiener Power-supply switched OFF
+fi
+
+exit $FAIL
--- user_scripts/wiener/mainswitchON.sh
+++ user_scripts/wiener/mainswitchON.sh
@@ -4,13 +4,16 @@
 
 WIENER_IP=172.23.16.179
 # note that you need to have WIENER-CRATE-MIB.txt installed in /usr/share/snmp/mibs
-# and that the first time you plug it in, you cant use this script to turn it on,
+# and that the first time you plug it in, you can't use this script to turn it on,
 # you must use the physical switch.
 
 echo Switching the WIENER Power Supply ON
 
-# read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
-
 snmpset -v 2c -m +WIENER-CRATE-MIB -c guru $WIENER_IP sysMainSwitch.0 i 1
 
-echo WIENER Power Supply switched ON
+FAIL=$?
+if [ $FAIL -eq 0 ]; then
+echo Wiener Power-supply switched ON
+fi
+
+exit $FAIL
